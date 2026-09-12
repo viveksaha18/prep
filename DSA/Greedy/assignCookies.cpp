bc457282a64477b4a513312af231baa8966ff43c
Vivek Saha <viveksaha096@gmail.com>
2026-09-12 06:40:02 +0000

    Started with greedy algos

---
+++ DSA/Greedy/assignCookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int l = 0; 
+        int r = 0;
+        while(l < g.size() && r < s.size()) {
+            if(g[l] <= s[r]) {
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+};
