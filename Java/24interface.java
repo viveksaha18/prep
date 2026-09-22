cdaa61e8535f5b798a762eb45ecb09b1269dd358
Vivek Saha <viveksaha096@gmail.com>
2026-09-22 14:01:01 +0000

    Interface Java

---
+++ Java/24interface.java
@@ -0,0 +1,29 @@
+// interface 
+interface Animal {
+    void sound();
+}
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("SOund");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}// interface 
+interface Animal {
+    void sound();
+}
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("SOund");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
